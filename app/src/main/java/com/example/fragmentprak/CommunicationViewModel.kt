28e29f0985ad73cac4ed24b6d6d557358e1ed28e
shafiraaaabcd <fitrianinda_shafira_30rpl@student.smktelkom-mlg.sch.id>
2023-03-07 13:43:34 +0700

    fragment

---
+++ app/src/main/java/com/example/fragmentprak/CommunicationViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.fragmentprak
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CommunicationViewModel  : ViewModel() {
+    private val mName = MutableLiveData<String>()
+    val name: LiveData<String>
+        get() = mName
+    fun setName(name: String) {
+        mName.value = name
+    }
+}
